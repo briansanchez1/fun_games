7faec05148c708b6e2c9c7730eddc680a8bf3ca9
briansanchez1 <115597244+briansanchez1@users.noreply.github.com>
2024-05-10 16:14:45 -0400

    added some preliminary functionality to cipher_game.jsx

--- fun-stuff/src/pages/cipher_game.jsx
+++ fun-stuff/src/pages/cipher_game.jsx
@@ -1,23 +1,49 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 const CipherGame = () => {
-  useEffect(() => {
-    document.title = "Cipher Game";
-  });
+  const [quote, setQuote] = useState("hello chatgpt, I am a user.");
+  const [filledIndexes, setFilledIndexes] = useState([]);
+
+  const isAlphabetCharacter = (char) => /[a-zA-Z]/.test(char);
+
+  const handleChange = (index, value) => {
+    if (isAlphabetCharacter(value) || value === "") {
+      setQuote((prevQuote) => {
+        const newQuote =
+          prevQuote.slice(0, index) + value + prevQuote.slice(index + 1);
+        return newQuote;
+      });
+      if (!filledIndexes.includes(index)) {
+        setFilledIndexes((prevIndexes) => [...prevIndexes, index]);
+      }
+    }
+  };
+
   return (
-    <body>
+    <Container maxWidth="xs">
       <h1>Cipher Text Game</h1>
-      <p>This is where I show the quote</p>
-
-      <p>
-        There also needs to be a few inputs that are formatted in a specific way
-        so that they are only one character, and a valid character
-      </p>
-
+      <p>{quote}</p>
+      <Grid container spacing={2}>
+        {quote.split("").map((char, index) => (
+          <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+            <TextField
+              key={index}
+              value={filledIndexes.includes(index) ? char : ""}
+              onChange={(e) =>
+                handleChange(index, e.target.value.trim().charAt(0))
+              }
+              inputProps={{ minLength: 1, maxLength: 1 }}
+              disabled={!isAlphabetCharacter(char)} // Disable non-alphabet characters in the original quote
+            />
+          </Grid>
+        ))}
+      </Grid>
 
       <a href="/">Dashboard</a>
-
-    </body>
+    </Container>
   );
 };
 
