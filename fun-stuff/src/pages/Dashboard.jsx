cd9aebe4e9929a1b3c1a48357324b3e079f6d953
briansanchez1 <115597244+briansanchez1@users.noreply.github.com>
2024-05-10 16:14:18 -0400

    updated dashboard to properly reflect its purpose

--- fun-stuff/src/pages/Dashboard.jsx
+++ fun-stuff/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import GameCards from "../components/game_cards";
-import { Container } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 
 const Dashboard = () => {
   useEffect(() => {
@@ -8,9 +8,20 @@ const Dashboard = () => {
   });
   return (
     <Container>
-      <GameCards />
-      <GameCards />
-      <GameCards />
+      <Typography variant="h3" mb={2}>
+        Brian's Favorite Games
+      </Typography>
+      <Grid container direction="row" spacing={1} >
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <GameCards gameName={"Cipher Game"} gameDesc={"This is a game about Deciphering some text that is scrambled."} gameLink={"cipher_game"}/>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <GameCards gameName={"Game 2"} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <GameCards gameName={"game 3"} />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
