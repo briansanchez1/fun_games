34b88c8513d9c5a03e5784ff6e6ea2b43a700bb3
briansanchez1 <sanchezmelob@wit.edu>
2024-05-10 19:52:55 -0400

    removed unneeded imports and formatted

--- fun-stuff/src/App.js
+++ fun-stuff/src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
 import CipherGame from "./pages/cipher_game";
 import Dashboard from "./pages/Dashboard";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -14,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/cipher_game" element={<CipherGame/>} />
+          <Route path="/cipher_game" element={<CipherGame />} />
         </Routes>
       </Router>
     </div>
