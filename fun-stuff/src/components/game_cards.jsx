bf8b67140bc2ebc434be13927412eef470589590
briansanchez1 <115597244+briansanchez1@users.noreply.github.com>
2024-05-11 21:09:07 -0400

    Added image for ciphe game, updated game card and dashboard styling

--- fun-stuff/src/components/game_cards.jsx
+++ fun-stuff/src/components/game_cards.jsx
@@ -3,17 +3,24 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
 
-const Game_Cards = ({ gameName, gameDesc, gameLink }) => {
+const Game_Cards = ({ gameName, gameDesc, gameLink, image }) => {
   return (
-    <Card sx={{borderRadius: "15px"}}>
+    <Card sx={{ borderRadius: "15px", border: "1px solid black" }}>
       <CardContent>
         <Typography sx={{ fontSize: 20 }}>{gameName}</Typography>
         <Typography sx={{ fontSize: 14 }}>{gameDesc}</Typography>
       </CardContent>
+      <CardMedia component="img" image={image} />
       <Button
         href={gameLink ? "/" + gameLink : "/"}
-        sx={{ justifyContent: "center" }}
+        sx={{
+          my: 1,
+          border: "1px solid black",
+          color: "black",
+          justifyContent: "center",
+        }}
       >
         Play
       </Button>
--- fun-stuff/src/pages/Dashboard.jsx
+++ fun-stuff/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import GameCards from "../components/game_cards";
 import { Container, Grid, Typography } from "@mui/material";
+import cipher from "../assets/Cipher.png";
 
 const Dashboard = () => {
   useEffect(() => {
     document.title = "Dashboard";
   });
+
   return (
     <Container>
       <Typography variant="h3" mb={2}>
@@ -16,8 +18,9 @@ const Dashboard = () => {
           <GameCards
             gameName={"Cipher Game"}
             gameDesc={
-              "This is a game about Deciphering some text that is scrambled."
+              "This is a game about deciphering a generated quote that has been scrambled using a substitution cipher method."
             }
+            image={cipher}
             gameLink={"cipher_game"}
           />
         </Grid>
